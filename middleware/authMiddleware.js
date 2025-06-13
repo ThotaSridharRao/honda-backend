bbf7f0c6bac22c768a8f6f25d4d064d17f0840a2
ThotaSridharRao <t.shridher@gmail.com>
2025-06-13 16:21:31 +0530

    made changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,11 @@ const jwt = require('jsonwebtoken');
 
 // Middleware to verify JWT token
 const authMiddleware = (req, res, next) => {
+  // Skip middleware for OPTIONS requests
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
+
   // Get token from header
   const token = req.header('x-auth-token');
 
@@ -12,13 +17,9 @@ const authMiddleware = (req, res, next) => {
 
   // Verify token
   try {
-    // Verify the token using your JWT secret.
-    // Replace 'your_jwt_secret' with a strong, unique secret key.
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Attach the user (from the token payload) to the request object
     req.user = decoded.user;
-    next(); // Move to the next middleware/route handler
+    next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
